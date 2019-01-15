dca243e3f92791c2fbbfd61f13485c0abe95941d
SchoolAccB <bbrands@roc-dev.com>
2019-01-15 13:34:31 +0100

    Commit

---
+++ H2/JavaScript/Opdracht21.js
@@ -0,0 +1,7 @@
+document.getElementById("paginatitel").innerText = "Dit is de paginatitel."
+
+document.getElementById("subkop").innerText = "Dit is de subkop."
+
+document.getElementById("foto1").src = "https://picsum.photos/200/300"
+
+document.getElementById("foto2").src = "https://picsum.photos/200"
---
+++ H2/JavaScript/Opdracht22.js
@@ -0,0 +1,3 @@
+bl1.style.backgroundColor = "deepskyblue"
+
+bl1.padding = "10px"
