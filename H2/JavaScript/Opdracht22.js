1171491a85db1685b1e0f57b7ce834fab00701f0
SchoolAccB <bbrands@roc-dev.com>
2019-01-21 14:12:57 +0100

    Commit

--- H2/JavaScript/Opdracht22.js
+++ H2/JavaScript/Opdracht22.js
@@ -1,3 +1,16 @@
-bl1.style.backgroundColor = "deepskyblue"
+bl1.style.backgroundColor = "Cyan";
+bl1.style.textAlign = "center";
+bl1.style.paddingTop = "20px";
 
-bl1.padding = "10px"
+bl2.style.backgroundColor = "DarkGreen";
+bl2.style.marginTop = "20px";
+bl2.style.textAlign = "center";
+
+bl3.style.marginTop = "60px";
+bl3.style.textAlign = "right";
+
+bl4.style.backgroundColor = "Red";
+bl4.style.textAlign = "Left";
+
+
+
